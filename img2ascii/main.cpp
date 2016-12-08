ce29f09f316edabdd6af0209e2ee135ad516f33f
ChenJiyang <silensmile@outlook.com>
2016-12-08 23:27:19 +0800

    修改描述

--- img2ascii/main.cpp
+++ img2ascii/main.cpp
@@ -5,14 +5,17 @@ using cv::imread;
 using cv::waitKey;
 
 int main(int argc,char* argv[]) {
+	system("title=img2ascii By silensmile");
 	if (argc == 1) {
 		std :: cout << "Please input the path of the image to be converted and run in the cmd" << std :: endl;
+		std::cout << "Example: img2ascii.exe ./image.png" << std::endl;
 		system("pause");
 		return 0;
 	}
 	const Mat src = imread(argv[1]);
 	Mat temp = preProcess(src);
 	img2ascii(temp);
+	std::cout << "Please use monospace fonts in notepad" << std::endl;
 	system("ascii.txt");
 	system("pause");
 	return 0;
